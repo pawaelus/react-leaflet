eafa4f85dbc6316157284c27fec9e95ef5a99347
Paweł Pisera <ppisera@e-point.pl>
2020-12-09 21:40:13 +0100

    Updates

---
+++ precache-manifest.067ad8c98bca9d5ba43a85dee8369496.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc4e12c139635e7c7b1efe857509f60",
+    "url": "/react-leaflet/index.html"
+  },
+  {
+    "revision": "4ea056f27887467babf9",
+    "url": "/react-leaflet/static/css/2.3c21e733.chunk.css"
+  },
+  {
+    "revision": "b2d7951edd0fd1f7d3a7",
+    "url": "/react-leaflet/static/css/main.53630c35.chunk.css"
+  },
+  {
+    "revision": "4ea056f27887467babf9",
+    "url": "/react-leaflet/static/js/2.37dda3c2.chunk.js"
+  },
+  {
+    "revision": "7414a271e33e63201c0af477443fe765",
+    "url": "/react-leaflet/static/js/2.37dda3c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d7951edd0fd1f7d3a7",
+    "url": "/react-leaflet/static/js/main.bf250484.chunk.js"
+  },
+  {
+    "revision": "f87385bdf36511a9c1ad",
+    "url": "/react-leaflet/static/js/runtime-main.3c402959.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-leaflet/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-leaflet/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-leaflet/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-leaflet/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-leaflet/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fd5728f2cf777b06b966d05c0c823dc9",
+    "url": "/react-leaflet/static/media/spritesheet.fd5728f2.svg"
+  }
+]);
